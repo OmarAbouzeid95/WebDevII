eb3a19f0f669dd0128cb331a5f456ce3e7c61487
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2023-04-09 01:26:00 -0400

    added productInfo.html and productInfo.js files

---
+++ productInfo.js
@@ -0,0 +1,54 @@
+
+// selectors
+const $ = (selector) => document.querySelector(selector)
+const productInfoContainer = $(".productInfo-container")
+
+
+
+// function to add product information to the page
+function updateProductInfo() {
+
+    // fetch from local storage the current product info
+    const item = JSON.parse(localStorage.getItem("productInfo"))
+
+    productInfoContainer.innerHTML = `<div class="productInfo-wrapper">
+                                        <div class="productUser-details">
+                                            <img class="productUser-pp" src=${item.profilePicture} alt="profile picture"/>
+                                            <h4 class="productInfo-username">${item.name}</h4>
+                                        </div>
+                                        <h3 class="productInfo-name">Model: ${item.productName}</h3>
+                                        <h4 class="productInfo-price">Price: $ ${item.productPrice}</h4>
+                                        <div class="productInfo-images">
+                                            <img class="productInfo-currentImg" src=${item.productImg} alt="current image">
+                                            <div class="productInfo-otherImgs">
+                                                <img class="productInfo-imgList" src=${item.productImgList[0]} alt="img 1">
+                                                <img class="productInfo-imgList" src=${item.productImgList[1]} alt="img 2">
+                                                <img class="productInfo-imgList" src=${item.productImgList[2]} alt="img 3">
+                                                <img class="productInfo-imgList" src=${item.productImgList[3]} alt="img 3">
+                                            </div>
+                                        </div>
+                                        <div class="productInfo-details">
+                                            <h4 class="productInfo-desc">Description</h4>
+                                            <p class="productInfo-description">${item.productDescription}</p>
+                                            <div class="productInfo-textArea">
+                                                <textarea class="productInfo-msg" placeholder="Send a message..."></textarea>
+                                                <img class="submitIcon" src="./images/sendIcon.png" alt="submit btn">
+                                            </div>
+                                        </div>
+                                    </div>`
+    
+    // updating the current image when another image from the list is clicked
+    const currentImg = $('.productInfo-currentImg')
+    const images = document.querySelectorAll(".productInfo-imgList")
+    images.forEach(img => {
+        img.addEventListener("click", (e) => {
+            // changing the src attribute of the currentImg
+            const src = e.target.attributes[1].value
+            currentImg.setAttribute("src", src)
+        })
+    })
+}
+
+// call function on page load
+window.onload = updateProductInfo()
+
--- script.js
+++ script.js
@@ -4,7 +4,6 @@ import database from './db.js'
 // Selecting product container to populate with data from the DB.
 const $ = (selector) => document.querySelector(selector)
 const productsContainer = $('.products-container')
-const productInfoContainer = $('.productInfo-container')
 const welcomeTitle = $('#welcome-title')
 const searchBtn = $('.searchIcon')
 const searchBar = $('#searchbar')
@@ -15,16 +14,9 @@ const loggedUser = localStorage.getItem("loggedUser")
 if (loggedUser)
     welcomeTitle.innerText = `Welcome, ${loggedUser}`
 
-// hiding the productInfo-container
-function hideProductInfo(){
-    productInfoContainer.classList.add("hidden")
-    productInfoContainer.innerHTML = ''
-}
-
 // populating the product-details container with data.
 function updateProductInfo(id){
     // toggle hide class from the productInfo-container
-    productInfoContainer.classList.remove("hidden")
     let item = {}
     for(let i = 0; i < database.length; i++){
         if(database[i].id === id){
@@ -32,45 +24,13 @@ function updateProductInfo(id){
             break
         }
     }
-    console.log(item)
-    productInfoContainer.classList.remove("hidden")
-    productInfoContainer.innerHTML = `<div class="productInfo-wrapper">
-                                        <img class="closeIcon" src="./images/closeIcon.png" alt="close icon"/>
-                                        <div class="productInfo-images">
-                                            <img class="productInfo-currentImg" src=${item.productImg} alt="current image">
-                                            <div class="productInfo-otherImgs">
-                                                <img class="productInfo-imgList" src=${item.productImgList[0]} alt="img 1">
-                                                <img class="productInfo-imgList" src=${item.productImgList[1]} alt="img 2">
-                                                <img class="productInfo-imgList" src=${item.productImgList[2]} alt="img 3">
-                                                <img class="productInfo-imgList" src=${item.productImgList[3]} alt="img 3">
-                                            </div>
-                                        </div>
-                                        <div class="productInfo-details">
-                                            <div class="productUser-details">
-                                                <img class="productUser-pp" src=${item.profilePicture} alt="profile picture"/>
-                                                <h4>${item.name}</h4>
-                                            </div>
-                                            <p class="productInfo-description">${item.productDescription}</p>
-                                            <div class="productInfo-textArea">
-                                                <textarea class="productInfo-msg" placeholder="Send a message..."></textarea>
-                                                <img class="submitIcon" src="./images/sendIcon.png" alt="submit btn">
-                                            </div>
-                                        </div>
-                                      </div>`
+
+    // set the product Info in the local storage
+    localStorage.setItem("productInfo", JSON.stringify(item))
+
+    // redirect to the productInfo.html page
+    location.href = "./productInfo.html"
     
-    // selecting the close icon
-    $('.closeIcon').addEventListener("click", hideProductInfo)
-    
-    // updating the current image when another image from the list is clicked
-    const currentImg = $('.productInfo-currentImg')
-    const images = document.querySelectorAll(".productInfo-imgList")
-    images.forEach(img => {
-        img.addEventListener("click", (e) => {
-            // changing the src attribute of the currentImg
-            const src = e.target.attributes[1].value
-            currentImg.setAttribute("src", src)
-        })
-    })
 }
 
 // function to populate the products container with products from the DB
